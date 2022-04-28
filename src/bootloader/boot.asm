23cd7d504a7ac7f66dec0f5ee92ba66af7475664
SaLtY Bacon <gaconbaming@gmail.com>
2022-04-28 12:08:21 -0230

    Updated files (see desc)
    Makefile
    src\bootloader\boot.asm
    test.txt
    tools\fat\fat.c

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -14,7 +14,7 @@ nop
 
 bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
 bdb_bytes_per_sector:       dw 512
-bdb_sectors_per_sector:     db 1
+bdb_sectors_per_cluster:    db 1
 bdb_reserved_sectors:       dw 1
 bdb_fat_count:              db 2
 bdb_dir_entries_count:      dw 0E0h
---
+++ tools/fat/fat.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
+
+typedef struct
+{
+    uint8_t BootJumpInstruction[3];
+    uint8_t OemIdentifier[8];
+    uint16_t BytesPerSector;
+    uint8_t SectorsPerCluster;
+    uint16_t ReservedSectors;
+    uint8_t FatCount;
+    uint16_t DirEntryCount;
+    uint16_t TotalSectors;
+    uint8_t MediaDescriptorType;
+    uint16_t SectorsPerFat;
+    uint16_t SectorsPerTrack;
+    uint16_t Heads;
+    uint32_t HiddenSectors;
+    uint32_t LargeSectorCount;
+
+    // extended boot record
+    uint8_t DriveNumber;
+    uint8_t _Reserved;
+    uint8_t Signature;
+    uint32_t VolumeId;          // serial number, value doesn't matter
+    uint8_t VolumeLabel[11];    // 11 bytes, padded withh space
+    uint8_t SystemId[8];
+
+    // ... we don't care about code ... 
+
+} __attribute__((packed)) BootSector;
+
+BootSector g_BootSector;
+
+bool readBootSector(FILE* disk)
+{
+    return fread(&g_bootSector, sizeof(g_BootSector), 1, disk);
+}
+
+bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut)
+{
+    bool ok = true;
+    ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET) == 0);
+    ok = ok && (fread(bufferOut, g_BootSector.BytesPerSector, count, disk) == count);
+    return ok;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 3) {
+        printf("Syntax: %s <disk image> <file name>\n", argv[0]);
+        return -1;
+    }
+
+    FILE* disk = fopen(argv[1], "rb");
+    if (!disk) {
+        fprintf(stderr, "Cannot open disk image %s!\n", argv[1]);
+        return -1;
+    }
+
+    if (!readBootSector(disk)) {
+        fprintf(stderr, "Coult not read boot sector!\n");
+        reeturn -2;
+    }
+
+    return 0;
+}
